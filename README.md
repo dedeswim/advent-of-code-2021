7e3024b39dcde126912d9931c59f0af93a9e2818
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2021-12-01 16:53:55 +0100

    Read from file instead of stdin

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Advent of Code 2021
 
 Advent of Code 2021 (my first one!), also an attempt to practice a bit of Rust.
+
+## Running
+
+(Assuming that the respective inputs are in `inputs/{day}`)
+
+### December 1st
+
+#### Ex. 1
+
+```sh
+cargo run --bin 01 -- inputs/01 1
+```
+
+#### Ex. 2
+
+```sh
+cargo run --bin 01 -- inputs/01 3
+```
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,18 +1,29 @@
-use std::io::{self, BufRead};
-
-const WINDOW: usize = 3;
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() {
     // Not the most efficient way to implement this, as the whole stdio stream gets read,
     // but I wanted to use a functional approach. An alternative could of course have
     // been to use a loop.
 
-    let total = io::stdin()
-        .lock()
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("Usage: {} input-file window-size", args[0]);
+    }
+
+    let filename = &args[1];
+    let window_size: usize = args[2].parse().expect("window size must be a number");
+
+    let file = File::open(filename).unwrap();
+
+    let total = BufReader::new(file)
         .lines()
         .map(|l| l.unwrap().parse().unwrap()) // Parse ints
         .collect::<Vec<i32>>()
-        .windows(WINDOW) // Create sliding window
+        .windows(window_size) // Create sliding window
         .map(|w| w.iter().sum::<i32>()) // Sum elements in sliding window
         .collect::<Vec<i32>>()
         .windows(2) // Create sliding window for comparison and compare
